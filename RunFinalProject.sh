db12c538e936a5958d25b340b992188fce8c3718
Nipun goyal <goya0016@algonquinlive.com>
2019-12-11 15:26:52 -0500

    finished step e

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -4,3 +4,4 @@ git init
 cp  /Library/WebServer/Documents/index.html  ~/documents/FinalProjectRepository/
 git add .
 git commit -m "step 5"
+curl -u "goya0016" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository","private":"true"}'
