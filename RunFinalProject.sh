fb4c1030466b2b679efa3b489629425b68bfaed5
Nipun goyal <goya0016@algonquinlive.com>
2019-12-11 15:28:34 -0500

    finished step g

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -6,3 +6,9 @@ git add .
 git commit -m "step 5"
 curl -u "goya0016" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository","private":"true"}'
 git push --set-upstream https://github.com/goya0016/FinalProjectRepository.git master
+ssh goya0016@52.149.186.16
+git clone https://github.com/goya0016/FinalProjectRepository.git
+cd FinalProjectRepository
+rm -rf .git
+sudo mv FinalProjectRepository/ /var/www/html
+sudo systemctl start nginx
