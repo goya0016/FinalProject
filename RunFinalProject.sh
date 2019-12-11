f48bed140ae159f7cc5f26c691b28dc69564df73
Nipun goyal <goya0016@algonquinlive.com>
2019-12-11 15:26:27 -0500

    finished step d

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -2,3 +2,5 @@ mkdir  ~/Documents/FinalProjectRepository
 cd ~/Documents/FinalProjectRepository/
 git init
 cp  /Library/WebServer/Documents/index.html  ~/documents/FinalProjectRepository/
+git add .
+git commit -m "step 5"
